e2035251aa4e94439316c4ab6ad45209ebbcd7e3
Rodrigo Gutierrez <rodrigo.gutierrez.k@gmail.com>
2016-04-23 00:54:24 -0500

    Prueba

--- rodrigogutierrezk/urls.py
+++ rodrigogutierrezk/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-#from testsite.home import views #agregada esta linea
 
 urlpatterns = [
     # Examples:
-    #url(r'^$', 'rodrigogutierrezk.views.home', name='home'),#descomentada esta linea
+    #url(r'^$', 'rodrigogutierrezk.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', include('home.urls')),
     url(r'^home/', include('home.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
